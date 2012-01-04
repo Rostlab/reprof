8275a6cc49db169a67c040a335cb3f0e78176dab
Laszlo Kajan <lkajan@rostlab.org>
2012-01-04 18:11:55 +0000

    Laszlo reorganizing debianization - do not worry
    git-svn-id: svn+ssh://i12k-biolab01.informatik.tu-muenchen.de/mnt/project/subversion/reprof/trunk@76 5a55f1d0-c592-4416-93ef-8ecf5fe701ca

--- deb/reprof-1.0/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(reprof):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="reprof" command="/usr/bin/reprof"
--- deb/reprof-1.0/debian/reprof.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the reprof package
-#
-0 4	* * *	root	[ -x /usr/bin/reprof_maintenance ] && /usr/bin/reprof_maintenance
